d48e27bd25acc55a80715bc24d963a589b127686
tejasm25 <43705567+tejasm25@users.noreply.github.com>
2018-10-09 20:22:09 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,12 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = int(input("enter the age: "))
+enter the age: 20
+>>> if a >= 20:
+	print("you can vote")
+else:
+	print("you cannot vote")
+
+	
+you can vote
+>>> 
